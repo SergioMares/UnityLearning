441e0da9075f74cb034fbad66ab9e2edd32e9086
Sergio Mares <sergiomaresmtz@gmail.com>
2022-02-04 11:11:31 -0600

    challenge2 completed

---
+++ JrProgrammer/Challenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float spawnDogDelay = 1,
+                tempoTime = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tempoTime <= 0)
+        {
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                tempoTime = spawnDogDelay;
+            }
+        }
+        else
+            tempoTime -= Time.deltaTime;
+    }
+}
